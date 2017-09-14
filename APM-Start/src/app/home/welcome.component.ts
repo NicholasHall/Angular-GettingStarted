f2ad638a4d735352aa095748cd060868b6955077
NicholasHall <nickhall16@gmail.com>
2017-09-14 15:21:46 -0300

    https://github.com/NicholasHall/Angular-GettingStarted.git

---
+++ APM-Start/src/app/home/welcome.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+//test
+
+@Component({
+    templateUrl: './welcome.component.html'
+})
+export class WelcomeComponent {
+    public pageTitle: string = 'Welcome';
+}
